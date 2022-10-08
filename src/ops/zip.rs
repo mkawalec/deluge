dbf12fe17ea6c90e63da16f61fe818e5c8c87c73
Michał Kawalec <michal@monad.cat>
2022-10-08 15:10:51 +0200

    saved zip

--- src/ops/zip.rs
+++ src/ops/zip.rs
@@ -1,33 +1,42 @@
 use crate::deluge::Deluge;
 use super::collect::Collect;
+use std::collections::VecDeque;
 use std::future::Future;
+use std::marker::PhantomData;
 use pin_project::pin_project;
 use std::pin::Pin;
 use futures::stream::StreamExt;
+use futures::join;
 
 
 #[pin_project]
-pub struct Zip<'a, Del1, Del2>
+pub struct Zip<Del1, Del2>
 where
-    Del1: Deluge<'a>,
-    Del2: Deluge<'a>,
+    Del1: Deluge,
+    Del2: Deluge,
 {
     #[pin]
-    first: Collect<'a, Del1, ()>,
+    first: Collect<Del1, ()>,
 
     #[pin]
-    second: Collect<'a, Del2, ()>,
+    second: Collect<Del2, ()>,
 
     finished: bool,
 }
 
-impl<'a, Del1, Del2> Zip<'a, Del1, Del2>
+impl<'a, Del1, Del2> Zip<Del1, Del2>
 where
-    Del1: Deluge<'a>,
-    Del2: Deluge<'a>,
+    Del1: Deluge,
+    Del2: Deluge,
 {
-    pub(crate) fn new(concurrency: impl Into<Option<usize>>, first: Del1, second: Del2) -> Self {
+    pub(crate) fn new(
+        first: Del1,
+        second: Del2,
+        concurrency: impl Into<Option<usize>>,
+    ) -> Self {
         let concurrency = concurrency.into().map(|conc| conc / 2);
+
+        // Preload the futures from each
 
         Self {
             first: Collect::new(first, concurrency.clone()),
@@ -37,30 +46,74 @@ where
     }
 }
 
-impl<'a, Del1, Del2> Deluge<'a> for Zip<'a, Del1, Del2>
+struct PreloadedFutures<Del>
+where Del: Deluge
+{
+    storage: VecDeque<Del::Output>,
+}
+
+impl<'a, Del> PreloadedFutures<Del>
+where Del: Deluge
+{
+    fn preload(mut deluge: Del) -> Self {
+        let mut storage = VecDeque::new();
+        while let Some(v) = deluge.next() {
+            storage.push_back(v);
+        }
+
+        Self {
+            storage,
+        }
+    }
+
+    fn len(&self) -> usize {
+        self.storage.len()
+    }
+}
+
+impl<'a, Del> Deluge for PreloadedFutures<Del>
+where Del: Deluge
+{
+    type Item = Del::Item;
+    type Output = Del::Output;
+
+    fn next(&'a mut self) -> Option<Self::Output> {
+        self.storage.pop_front()
+    }
+}
+
+impl<Del1, Del2> Deluge for Zip<Del1, Del2>
 where
-    Del1: Deluge<'a> + 'a,
-    Del2: Deluge<'a> + 'a,
+    Del1: Deluge,
+    Del2: Deluge,
 {
     type Item = (Del1::Item, Del2::Item);
     type Output = impl Future<Output = Option<Self::Item>> + 'a;
 
-    fn next(&'a mut self) -> Option<Self::Output> {
+    fn next(&mut self) -> Option<Self::Output> {
+        println!("Next entered");
         if self.finished {
+            println!("finishd");
             None
         } else {
+            println!("returning a promise");
             Some(async move {
-                let this = Pin::new(self).project();
-                // Get the first nonempty element from `first`, first nonempty from `second`.
-                // If none can be found on either, return a filtered element and then None.
-                if Some(el) = this.first.next().await {
+                println!("returning a future");
+                let mut this = Pin::new(self).project();
+                println!("About to wait");
+                let (first_el, second_el) = join!(
+                    this.first.next(),
+                    this.second.next()
+                );
 
-                } else {
+                if first_el.is_none() || second_el.is_none() {
+                    println!("We're finished");
                     *this.finished = true;
-                    return None;
+                    None
+                } else {
+                    println!("Resutning");
+                    Some((first_el.unwrap(), second_el.unwrap()))
                 }
-
-                todo!()
             })
         }
     }
